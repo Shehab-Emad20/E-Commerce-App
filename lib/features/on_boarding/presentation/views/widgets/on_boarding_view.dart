5b4422af0c2b9cf1e29fc86eac0940e6f074dd45
Shehab-Emad20 <shehabz2014@gmail.com>
2025-01-30 20:44:50 +0200

    Page View item part two

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/utils/app_images.dart';
+import 'package:flutter_application_1/features/on_boarding/presentation/views/widgets/page_view_item.dart';
 
 class OnBoardingPageView extends StatelessWidget {
   const OnBoardingPageView({super.key});
@@ -6,7 +8,28 @@ class OnBoardingPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: [],
+      children: [
+        PageViewItem(
+          backgroundImage: Assets.imagesPageViewItem1BackgroundImage,
+          image: Assets.imagesPageViewItem1Image,
+          subtitle:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'مرحبًا بك في',
+              ),
+              Text(
+                '  HUB',
+              ),
+              Text(
+                'Fruit',
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/helper_function/on_generate_routes.dart';
 import 'package:flutter_application_1/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_application_1/generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(FrutisHub());
@@ -12,6 +14,14 @@ class FrutisHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: Locale('ar'),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoutes,
       initialRoute: SplashView.routeName,

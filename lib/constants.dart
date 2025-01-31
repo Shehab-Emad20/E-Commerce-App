09e2cf504732c86319eb1021f3a8a6bb840500c7
Shehab-Emad20 <shehabz2014@gmail.com>
2025-01-31 16:45:37 +0200

    Custom Button

---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+const khorizinatalpadding = 16.0;
--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,4 +1,8 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/core/utils/app_colors.dart';
+import 'package:flutter_application_1/core/utils/widgets/custom_button.dart';
 import 'package:flutter_application_1/features/on_boarding/presentation/views/widgets/on_boarding_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -7,7 +11,19 @@ class OnBoardingViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Expanded(child: OnBoardingPageView())],
+      children: [
+        Expanded(child: OnBoardingPageView()),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(activeColor: AppColors.primaryColor),
+        ),
+        SizedBox(height: 29),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: khorizinatalpadding),
+          child: CustomButton(onPressed: () {}, text: 'أبدأ الان'),
+        ),
+        SizedBox(height: 43),
+      ],
     );
   }
 }

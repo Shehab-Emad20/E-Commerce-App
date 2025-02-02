f028848ccbc600c2558427121bf9e973bc6e5108
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-02 18:09:26 +0200

    Signup ui part 2

--- lib/features/auth/presentation/view/widgets/singup_view_body.dart
+++ lib/features/auth/presentation/view/widgets/singup_view_body.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/core/utils/widgets/custom_text_field.dart';
 
 class SingnupViewBody extends StatelessWidget {
   const SingnupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: khorizinatalpadding),
+        child: Column(children: [
+          SizedBox(height: 24),
+          CustomTextFormField(
+              hintText: 'الاسم الكامل', textInputType: TextInputType.name),
+          SizedBox(height: 16),
+          CustomTextFormField(
+              hintText: 'البريد الالكتروني ',
+              textInputType: TextInputType.emailAddress),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: 'كلمة المرور',
+            textInputType: TextInputType.visiblePassword,
+            suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
+          ),
+        ]),
+      ),
+    );
   }
 }

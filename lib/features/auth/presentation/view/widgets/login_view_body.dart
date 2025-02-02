c5d31d4405da1a2b40cbf4fb438e10da8070d5b4
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-02 15:20:37 +0200

    Login ui part 4

--- lib/features/auth/presentation/view/widgets/login_view_body.dart
+++ lib/features/auth/presentation/view/widgets/login_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/core/utils/app_colors.dart';
+import 'package:flutter_application_1/core/utils/app_text_styles.dart';
+import 'package:flutter_application_1/core/utils/widgets/custom_button.dart';
 import 'package:flutter_application_1/core/utils/widgets/custom_text_field.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -17,11 +20,36 @@ class LoginViewBody extends StatelessWidget {
               hintText: 'البريد الالكتروني',
               textInputType: TextInputType.emailAddress,
             ),
+            SizedBox(height: 16),
             CustomTextFormField(
               suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
               hintText: 'كلمة المرور',
               textInputType: TextInputType.visiblePassword,
             ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'نسيت كلمة المرور ؟',
+                  style: TextStyles.semiBold13
+                      .copyWith(color: AppColors.lightprimaryColor),
+                ),
+              ],
+            ),
+            SizedBox(height: 33),
+            CustomButton(text: 'تسجيل الدخول', onPressed: () {}),
+            SizedBox(height: 33),
+            Text.rich(TextSpan(children: [
+              TextSpan(
+                  text: 'ليس لديك حساب ؟',
+                  style:
+                      TextStyles.semiBold16.copyWith(color: Color(0xFF949D9E))),
+              TextSpan(
+                  text: ' إنشاء حساب',
+                  style: TextStyles.semiBold16
+                      .copyWith(color: AppColors.primaryColor)),
+            ]))
           ],
         ),
       ),

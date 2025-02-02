72f2fd42db3230b5cd88a2fe0f5d502919bef412
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-02 21:28:00 +0200

    Signup ui part 4

--- lib/features/auth/presentation/view/widgets/terms_and_conditions.dart
+++ lib/features/auth/presentation/view/widgets/terms_and_conditions.dart
@@ -2,48 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants.dart';
 import 'package:flutter_application_1/core/utils/app_colors.dart';
 import 'package:flutter_application_1/core/utils/app_text_styles.dart';
+import 'package:flutter_application_1/features/auth/presentation/view/widgets/custom_check_box.dart';
 
-class TermsAndConditions extends StatelessWidget {
+class TermsAndConditions extends StatefulWidget {
   const TermsAndConditions({super.key});
 
   @override
+  State<TermsAndConditions> createState() => _TermsAndConditionsState();
+}
+
+class _TermsAndConditionsState extends State<TermsAndConditions> {
+  bool isTermsAccepted = false;
+  @override
   Widget build(BuildContext context) {
-    return Transform.translate(
-      offset: const Offset(14, 0),
-      child: Row(
-        children: [
-          Checkbox(
-            value: true,
-            onChanged: (value) {},
-          ),
-          SizedBox(
-            width: MediaQuery.sizeOf(context).width -
-                (khorizinatalpadding * 2) -
-                48,
-            child: Text.rich(TextSpan(children: [
-              TextSpan(
-                text: 'من خلال إنشاء حساب، فإنك توافق على',
-                style: TextStyles.semiBold13.copyWith(color: Color(0xFF949D9E)),
-              ),
-              TextSpan(
-                text: ' الشروط والاحكام',
-                style: TextStyles.semiBold13
-                    .copyWith(color: AppColors.lightprimaryColor),
-              ),
-              TextSpan(
-                  text: '  الخاصة',
-                  style: TextStyles.semiBold13.copyWith(
-                    color: AppColors.lightprimaryColor,
-                  )),
-              TextSpan(
-                text: ' بنا ',
-                style: TextStyles.semiBold13
-                    .copyWith(color: AppColors.lightprimaryColor),
-              ),
-            ])),
-          )
-        ],
-      ),
+    return Row(
+      children: [
+        CustomCheckBox(
+            onChecked: (value) {
+              isTermsAccepted = value;
+              setState(() {});
+            },
+            isChecked: isTermsAccepted),
+        SizedBox(width: 16),
+        Expanded(
+          child: Text.rich(TextSpan(children: [
+            TextSpan(
+              text: 'من خلال إنشاء حساب، فإنك توافق على',
+              style: TextStyles.semiBold13.copyWith(color: Color(0xFF949D9E)),
+            ),
+            TextSpan(
+              text: ' الشروط والاحكام',
+              style: TextStyles.semiBold13
+                  .copyWith(color: AppColors.lightprimaryColor),
+            ),
+            TextSpan(
+                text: '  الخاصة',
+                style: TextStyles.semiBold13.copyWith(
+                  color: AppColors.lightprimaryColor,
+                )),
+            TextSpan(
+              text: ' بنا ',
+              style: TextStyles.semiBold13
+                  .copyWith(color: AppColors.lightprimaryColor),
+            ),
+          ])),
+        )
+      ],
     );
   }
 }

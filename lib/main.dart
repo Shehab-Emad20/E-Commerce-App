7c70cf5d4740a9cd54ec37487bfb3794c1148051
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-02 22:04:55 +0200

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/helper_function/on_generate_routes.dart';
 import 'package:flutter_application_1/core/services/shared_preferences_singleton.dart';
 import 'package:flutter_application_1/core/utils/app_colors.dart';
 import 'package:flutter_application_1/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_application_1/generated/l10n.dart'; // Ensure this import exists for localization
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(FrutisHub());
 }

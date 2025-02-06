3a354d4246628c9b8962d897743650ea685ac846
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-06 15:25:08 +0200

    Create Add User Data Method

--- lib/features/auth/domain/entites/user_entity.dart
+++ lib/features/auth/domain/entites/user_entity.dart
@@ -1,8 +1,16 @@
-///مهم جدا 
+///مهم جدا
 class UserEntity {
   final String email;
   final String name;
   final String uId;
 
   UserEntity({required this.email, required this.name, required this.uId});
+
+  toMap() {
+    return {
+      'email': email,
+      'name': name,
+      'uId': uId,
+    };
+  }
 }
--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -13,5 +13,7 @@ abstract class AuthRepo {
       String email, String password);
 
   Future<Either<Failure, UserEntity>> signInWithGoogle();
-    Future<Either<Failure, UserEntity>> signInWithFacebook();
+  Future<Either<Failure, UserEntity>> signInWithFacebook();
+
+  Future addDate({required UserEntity user});
 }

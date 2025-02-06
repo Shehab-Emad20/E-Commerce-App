515020333c85b13d3685c71ca3b28ab4a4d1fa18
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-06 14:55:05 +0200

    Create Abstract Database Service

--- lib/core/services/firestore._services.dart
+++ lib/core/services/firestore._services.dart
@@ -1,1 +1,13 @@
-class FirestoreServices {}
+import 'package:flutter_application_1/core/services/data_services.dart';
+
+class FirestoreServices implements DatabaseService {
+  @override
+  Future<void> addData({required String path, required Map<String, dynamic> data}) {
+    
+    throw UnimplementedError();
+  }
+
+   
+}
+
+

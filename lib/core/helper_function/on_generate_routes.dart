8c08a482bfc7646a5be3a9233c7d73d3cff53e51
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-07 17:08:45 +0200

    Create home feature

--- lib/core/helper_function/on_generate_routes.dart
+++ lib/core/helper_function/on_generate_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/features/auth/presentation/view/sign_in.dart';
 import 'package:flutter_application_1/features/auth/presentation/view/singnup_view.dart';
+import 'package:flutter_application_1/features/home/presentation/views/home_view.dart';
 import 'package:flutter_application_1/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter_application_1/features/splash/presentation/views/splash_view.dart';
 
@@ -17,6 +18,9 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
 
     case SingnupView.routeName:
       return MaterialPageRoute(builder: (context) => const SingnupView());
+
+    case HomeView.routeName:
+      return MaterialPageRoute(builder: (context) => const HomeView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
--- lib/features/auth/presentation/view/widgets/signin_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/view/widgets/signin_view_body_bloc_consumer.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_1/core/helper_function/build_error_bar.dart'
 import 'package:flutter_application_1/core/utils/widgets/custom_progress_hUD.dart';
 import 'package:flutter_application_1/features/auth/presentation/cubits/sign_in_cubit/signin_cubit.dart';
 import 'package:flutter_application_1/features/auth/presentation/view/widgets/sign_in_view_body.dart';
+import 'package:flutter_application_1/features/home/presentation/views/home_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SigninViewBodyBlocConsumer extends StatelessWidget {
@@ -14,7 +15,9 @@ class SigninViewBodyBlocConsumer extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<SigninCubit, SigninState>(
       listener: (context, state) {
-        if (state is SigninSuccess) {}
+        if (state is SigninSuccess) {
+          Navigator.pushNamed(context, HomeView.routeName);
+        }
         if (state is SigninFailure) {
           buiddErrorBar(context, state.message);
         }

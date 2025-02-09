8e0dbca321a878ae15481a53ae2cd5c7680f81b8
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-09 17:47:38 +0200

    Create custom bottom navigation bar part 4

--- lib/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/core/utils/app_images.dart';
+
+import 'package:flutter_application_1/features/home/domain/entites/bottom_navigation_bar_entity.dart';
+import 'package:flutter_application_1/features/home/presentation/views/widgets/naivgation_bar_item.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
@@ -8,58 +10,34 @@ class CustomBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 357,
-      height: 70,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Color(0x19000000),
-            blurRadius: 25,
-            offset: Offset(0, -2),
-            spreadRadius: 0,
+        width: 357,
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
           ),
-        ],
-      ),
-      child: Row(
-        
-      )
-    );
+          boxShadow: [
+            BoxShadow(
+              color: Color(0x19000000),
+              blurRadius: 25,
+              offset: Offset(0, -2),
+              spreadRadius: 0,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: bottomNavigationBarItems.map((e) {
+            return NaivgationBarItem(
+                bottomNavigationBarEntity: e, isSelected: false);
+          }).toList(),
+        ));
   }
 }
 
-class InActiveItem extends StatelessWidget {
-  const InActiveItem({super.key, required this.image});
 
-  final String image;
 
-  @override
-  Widget build(BuildContext context) {
-    return SvgPicture.asset(image);
-  }
-}
 
-class NaivgationBarItem extends StatelessWidget {
-  const NaivgationBarItem({super.key, required this.isSelected});
 
-  final bool isSelected;
-  @override
-  Widget build(BuildContext context) {
-    return isSelected
-        ? ActiveItem()
-        : InActiveItem(image: Assets.imagesOutlineHome1);
-  }
-}
-
-class ActiveItem extends StatelessWidget {
-  const ActiveItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}

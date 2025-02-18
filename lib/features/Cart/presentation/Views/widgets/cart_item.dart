347af7d27c5d107d07395731bbf4c11d9fd79f70
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-18 18:58:56 +0200

    Create cart view part 3

--- lib/features/Cart/presentation/Views/widgets/cart_item.dart
+++ lib/features/Cart/presentation/Views/widgets/cart_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/utils/app_colors.dart';
 import 'package:flutter_application_1/core/utils/app_images.dart';
 import 'package:flutter_application_1/core/utils/app_text_styles.dart';
 import 'package:flutter_application_1/core/utils/widgets/custom_image_network.dart';
+import 'package:flutter_application_1/features/Cart/presentation/Views/widgets/cart_item_action_buttons.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CartItem extends StatelessWidget {
@@ -26,14 +28,27 @@ class CartItem extends StatelessWidget {
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Row(
                   children: [
                     Text('بطيخ', style: TextStyles.bold13),
                     Spacer(),
-                    IconButton(
-                        onPressed: () {},
-                        icon: SvgPicture.asset(Assets.imagesTrash))
+                    GestureDetector(
+                        onTap: () {},
+                        child: SvgPicture.asset(Assets.imagesTrash))
+                  ],
+                ),
+                Text('3كم',
+                    style: TextStyles.regular13
+                        .copyWith(color: AppColors.secondaryColor)),
+                Row(
+                  children: [
+                    CartItemActionButtons(),
+                    Spacer(),
+                    Text('20 جنية',
+                        style: TextStyles.bold16
+                            .copyWith(color: AppColors.secondaryColor)),
                   ],
                 )
               ],

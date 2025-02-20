497a14c67780208401d7738326e44ad264788cc6
Shehab-Emad20 <shehabz2014@gmail.com>
2025-02-20 20:27:22 +0200

    Create checkout page view

--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/utils/widgets/custom_button.dart';
+import 'package:flutter_application_1/features/checkout/presentation/views/widgets/check_out_steps_page_view.dart';
 import 'package:flutter_application_1/features/checkout/presentation/views/widgets/checkout_steps.dart';
 
-class CheckoutViewBody extends StatelessWidget {
+class CheckoutViewBody extends StatefulWidget {
   const CheckoutViewBody({super.key});
+
+  @override
+  State<CheckoutViewBody> createState() => _CheckoutViewBodyState();
+}
+
+class _CheckoutViewBodyState extends State<CheckoutViewBody> {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
-        children: [SizedBox(height: 16), CheckoutSteps()],
+        children: [
+          SizedBox(height: 20),
+          CheckoutSteps(),
+          Expanded(
+            child: CheckoutStepsPageView(
+              pageController: pageController),
+          ),
+          CustomButton(onPressed: () {}, text: 'التالي'),
+          SizedBox(height: 32),
+        ],
       ),
     );
   }
